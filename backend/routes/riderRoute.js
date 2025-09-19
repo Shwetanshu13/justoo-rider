da3be5060afe700d150469b136086a7b8e1c3506
Shwetanshu13 <shwetanshusinha13@gmail.com>
2025-09-19 12:51:54 +0530

    Changed the location of the rider backend from rider to monorepo

--- backend/routes/riderRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/profile", getProfile);
-router.put("/updateprofile", authenticateToken, updateProfile);
-router.put("/updatepassword", authenticateToken, updatePassword);
